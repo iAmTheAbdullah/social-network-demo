8ea57e77adcdc6ce1bf854823860c4f8923b90d7
Abdullah Malik <asmalik250@gmail.com>
2020-04-14 20:38:01 +0100

    Initialise spring boot application

---
+++ server/src/main/java/com/iamtheabdullah/socialnetworkdemo/SocialNetworkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.iamtheabdullah.socialnetworkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkDemoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/iamtheabdullah/socialnetworkdemo/SocialNetworkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamtheabdullah.socialnetworkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
